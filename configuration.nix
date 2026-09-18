5e121f7873cfa0674e138b47ebb449ffc7f744a1
ilovesnailgirls <your-email@example.com>
2026-09-18 07:28:37 +0100

    Generation 163 (2026-09-18 07:28:37)

--- configuration.nix
+++ configuration.nix
@@ -123,6 +123,7 @@
 
   # PROGRAMS
   programs.fish.enable = true;
+  programs.starship.enable = true;
   programs.zsh.enable = true;
   virtualisation.waydroid.enable = true;
   programs.appimage = {
